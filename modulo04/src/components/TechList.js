ec016756ae9e24e539599f30163d41b33b184c6e
Rodrigo Ferraro <ferraro.rodrigo@gmail.com>
2020-02-05 11:55:42 -0300

    primeiro componente - simples

---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class TechList extends Component {
+
+  render(){
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>ReactJS</li>
+        <li>React Native</li>
+      </ul>
+    )
+  }
+}
+
+export default TechList;
